ebd65e2fadb27073447d3ece551da2c39c4a334b
Ariane Santos <ariane225040@gmail.com>
2024-08-30 10:51:01 -0300

    implementando métodos

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Desafio de Projeto Dio
+# Desafio de Projeto Dio 
+
+Sugestões:
+- Estender o domínio, incluindo novas classes, atributos e/ou métodos;
+- Incluir o Lombok ao projeto para reduzir sua verbosidade de código.
--- src/Conta.java
+++ src/Conta.java
@@ -6,28 +6,28 @@ public abstract class Conta implements IConta {
 	protected int agencia;
 	protected int numero;
 	protected double saldo;
+	protected Cliente cliente;
 
-	public Conta() {
+	public Conta(Cliente cliente) {
 		this.agencia = AGENCIA_PADRAO;
 		this.numero = SEQUENCIAL++;
+		this.cliente = cliente;
 	}
 	
 	@Override
 	public void sacar(double valor) {
-		// TODO Auto-generated method stub
-		
+		saldo -= valor;
 	}
 
 	@Override
 	public void depositar(double valor) {
-		// TODO Auto-generated method stub
-		
+		saldo += valor;
 	}
 
 	@Override
 	public void transferir(double valor, Conta contaDestino) {
-		// TODO Auto-generated method stub
-		
+		this.sacar(valor);
+		contaDestino.depositar(valor);
 	}
 
 	public int getAgencia() {
@@ -42,5 +42,10 @@ public abstract class Conta implements IConta {
 		return saldo;
 	}
 
-	
+	protected void imprimirInfosComuns() {
+		System.out.println(String.format("Titular: %s ", this.cliente.getNome()));
+		System.out.println(String.format("Agencia: %d ", this.agencia));
+		System.out.println(String.format("Numero: %d ", this.numero));
+		System.out.println(String.format("Saldo: %.2f ", this.saldo));
+	}
 }
