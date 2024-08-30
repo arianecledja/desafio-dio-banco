d6da302f0200ef08dc7811170387e7e613063758
Ariane Santos <ariane225040@gmail.com>
2024-08-30 09:35:53 -0300

    implementando o encapsulamento

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,13 +1,25 @@
 
 public class ContaPoupanca {
-	public int agencia;
-	public int numero;
-	public double saldo;
+	private int agencia;
+	private int numero;
+	private double saldo;
 	
 	public void sacar() {
 		
 	}
 	
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
 	public void depositar() {
 		
 	}
